a41d22f4bb06344ea2fcb4426fc1a35a4da8ada5
unknown <arhanansari2009@gmail.com>
2024-12-07 21:18:00 +0530

    Solving bugs

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,6 +1,7 @@
 // File Location: lib/appwrite/index.ts
 import { Client, Account, Databases, Storage } from "appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
+import { cookies } from "next/headers";
 
 const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
@@ -9,5 +10,45 @@ const client = new Client()
 const account = new Account(client);
 const databases = new Databases(client);
 const storage = new Storage(client);
+
+export const createSessionClient = async () => {
+    const client = new Client()
+      .setEndpoint(appwriteConfig.endpointUrl)
+      .setProject(appwriteConfig.projectId);
+  
+    const session = (await cookies()).get("appwrite-session");
+  
+    if (!session || !session.value) throw new Error("No session");
+  
+    client.setSession(session.value);
+  
+    return {
+      get account() {
+        return new Account(client);
+      },
+      get databases() {
+        return new Databases(client);
+      },
+    };
+  };
+  
+  export const createAdminClient = async () => {
+    const client = new Client()
+      .setEndpoint(appwriteConfig.endpointUrl)
+      .setProject(appwriteConfig.projectId)
+      .setKey(appwriteConfig.secretKey);
+  
+    return {
+      get account() {
+        return new Account(client);
+      },
+      get databases() {
+        return new Databases(client);
+      },
+      get storage() {
+        return new Storage(client);
+      },
+    };
+  };
 
 export { client, account, databases, storage };
