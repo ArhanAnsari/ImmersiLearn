f11c5ad1d619a79108ec2f99e0a2447144eb281d
unknown <arhanansari2009@gmail.com>
2024-12-14 19:27:28 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from 'next/font/google'
+import { ThemeProvider } from "@/components/theme-provider"
 
 import "./globals.css";
 
@@ -24,7 +25,14 @@ export default function RootLayout({
       <body
         className={`${poppins.variable} font-poppins antialiased`}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
